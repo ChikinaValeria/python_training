e97ef2b1f1c7cf434d3d926ce476f0f6421a0b6c
Veleria <dliavajnogo@gmail.com>
2024-01-16 12:05:57 +0300

    Тест отредактирован, запускается из командной строки

--- test_add_group.py
+++ test_add_group.py
@@ -1,41 +1,39 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.webdriver.firefox.webdriver import WebDriver
+
 import unittest, time, re
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
 
     
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(u"Моя первая группа")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(u"Хорошая группа")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(u"Нужная группа")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
+        wd.find_element('name',"user").clear()
+        wd.find_element('name',"user").send_keys("admin")
+        wd.find_element('name',"pass").click()
+        wd.find_element('name',"pass").clear()
+        wd.find_element('name',"pass").send_keys("secret")
+        wd.find_element('xpath', "//input[@value='Login']").click()
+        wd.find_element('link text',"groups").click()
+        wd.find_element('name',"new").click()
+        wd.find_element('name',"group_name").click()
+        wd.find_element('name',"group_name").clear()
+        wd.find_element('name',"group_name").send_keys("Моя первая группа")
+        wd.find_element('name',"group_header").click()
+        wd.find_element('name',"group_header").clear()
+        wd.find_element('name',"group_header").send_keys("Хорошая группа")
+        wd.find_element('name',"group_footer").click()
+        wd.find_element('name',"group_footer").clear()
+        wd.find_element('name',"group_footer").send_keys("Нужная группа")
+        wd.find_element('name',"submit").click()
+        wd.find_element('link text',"group page").click()
+        wd.find_element('link text', "Logout").click()
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
