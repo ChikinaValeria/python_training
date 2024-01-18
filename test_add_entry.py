aff7d047c491056d3b561e948132473851156585
Veleria <dliavajnogo@gmail.com>
2024-01-18 13:02:09 +0300

    parameters extracted

--- test_add_entry.py
+++ test_add_entry.py
@@ -12,52 +12,41 @@ class TestAddEntry(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/group.php")
 
-    def login(self, wd):
+    def login(self, wd, password, username):
         wd.find_element('name', "user").click()
         wd.find_element('name', "user").clear()
-        wd.find_element('name', "user").send_keys("admin")
+        wd.find_element('name', "user").send_keys(username)
         wd.find_element('name', "pass").click()
         wd.find_element('name', "pass").clear()
-        wd.find_element('name', "pass").send_keys("secret")
+        wd.find_element('name', "pass").send_keys(password)
         wd.find_element('xpath', "//input[@value='Login']").click()
 
-    def open_entry_page(self, wd):
-        wd.find_element('link text', "add new").click()
 
-    def create_entry(self, wd):
+    def create_entry(self, wd, firstname, lastname, address, home, mobile, email, email2, byear, ayear):
+        #init entry creation
+        wd.find_element('link text', "add new").click()
+        #fill entry form
         wd.find_element('name', "firstname").click()
         wd.find_element('name', "firstname").clear()
-        wd.find_element('name', "firstname").send_keys(u"Павел")
-        wd.find_element('name', "middlename").click()
-        wd.find_element('name', "middlename").clear()
-        wd.find_element('name', "middlename").send_keys(u"Арсений")
+        wd.find_element('name', "firstname").send_keys(firstname)
         wd.find_element('name', "lastname").click()
         wd.find_element('name', "lastname").clear()
-        wd.find_element('name', "lastname").send_keys(u"Петровский")
-        wd.find_element('name', "title").click()
-        wd.find_element('name', "title").clear()
-        wd.find_element('name', "title").send_keys("QA engineer")
-        wd.find_element('name', "company").click()
-        wd.find_element('name', "company").clear()
-        wd.find_element('name', "company").send_keys("Epam")
+        wd.find_element('name', "lastname").send_keys(lastname)
         wd.find_element('name', "address").click()
         wd.find_element('name', "address").clear()
-        wd.find_element('name', "address").send_keys("Russia, Saint-Petersburg")
+        wd.find_element('name', "address").send_keys(address)
         wd.find_element('name', "home").click()
         wd.find_element('name', "home").clear()
-        wd.find_element('name', "home").send_keys("1234578")
+        wd.find_element('name', "home").send_keys(home)
         wd.find_element('name', "mobile").click()
         wd.find_element('name', "mobile").clear()
-        wd.find_element('name', "mobile").send_keys("8 921 921 92 92")
+        wd.find_element('name', "mobile").send_keys(mobile)
         wd.find_element('name', "email").click()
         wd.find_element('name', "email").clear()
-        wd.find_element('name', "email").send_keys("pavel@gmail.com")
-        wd.find_element('name', "homepage").click()
-        wd.find_element('name', "homepage").clear()
-        wd.find_element('name', "homepage").send_keys("pavel.com")
+        wd.find_element('name', "email").send_keys(email)
         wd.find_element('name', "email2").click()
         wd.find_element('name', "email2").clear()
-        wd.find_element('name', "email2").send_keys("pavel1@gmail.com")
+        wd.find_element('name', "email2").send_keys(email2)
         wd.find_element('name', "bday").click()
         Select(wd.find_element('name', "bday")).select_by_visible_text("1")
         wd.find_element('xpath', "//option[@value='1']").click()
@@ -68,7 +57,7 @@ class TestAddEntry(unittest.TestCase):
         wd.find_element('xpath', "//option[@value='May']").click()
         wd.find_element('name', "byear").click()
         wd.find_element('name', "byear").clear()
-        wd.find_element('name', "byear").send_keys("1985")
+        wd.find_element('name', "byear").send_keys(byear)
         wd.find_element('name', "aday").click()
         Select(wd.find_element('name', "aday")).select_by_visible_text("16")
         wd.find_element('xpath', "//div[@id='content']/form/select[3]/option[18]").click()
@@ -77,7 +66,8 @@ class TestAddEntry(unittest.TestCase):
         wd.find_element('xpath', "//div[@id='content']/form/select[4]/option[11]").click()
         wd.find_element('name', "ayear").click()
         wd.find_element('name', "ayear").clear()
-        wd.find_element('name', "ayear").send_keys("1985")
+        wd.find_element('name', "ayear").send_keys(ayear)
+        #submit entry creation
         wd.find_element('xpath', "//input[20]").click()
 
     def return_to_the_entry_list(self, wd):
@@ -89,14 +79,12 @@ class TestAddEntry(unittest.TestCase):
     def test_add_entry(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.open_entry_page(wd)
-        self.create_entry(wd)
+        self.login(wd, password = "secret", username = "admin")
+        self.create_entry(wd, firstname = "Павел", lastname = "Петровский", address =  "Russia, Saint-Petersburg", home = "1234578", mobile = "8 921 921 92 92",
+                          email = "pavel@gmail.com", email2 = "pavel1@gmail.com", byear = "1985", ayear = "1985")
         self.return_to_the_entry_list(wd)
         self.logout(wd)
 
-    def return_to_the_entry_list(self, wd):
-        wd.find_element('id', "logo").click()
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
