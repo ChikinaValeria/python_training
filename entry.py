050e21ee759277730aee000592a63268a48d0415
Veleria <dliavajnogo@gmail.com>
2024-01-18 17:31:13 +0300

    parameter object extracted

---
+++ entry.py
@@ -0,0 +1,19 @@
+
+class Entry:
+    def __init__(self,firstname, lastname, address, home, mobile,
+                 email, email2, bday, bmonth, byear, aday, amonth, ayear):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.email = email
+        self.email2 = email2
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+
+
--- test_add_entry.py
+++ test_add_entry.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
 import unittest
 from selenium.webdriver.support.select import Select
-
+from entry import Entry
 class TestAddEntry(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
@@ -22,52 +22,51 @@ class TestAddEntry(unittest.TestCase):
         wd.find_element('xpath', "//input[@value='Login']").click()
 
 
-    def create_entry(self, wd, firstname, lastname, address, home, mobile, email, email2, byear, ayear, bmonth, aday,
-                     bday, amonth):
+    def create_entry(self, wd, entry):
         #init entry creation
         wd.find_element('link text', "add new").click()
         #fill entry form
         wd.find_element('name', "firstname").click()
         wd.find_element('name', "firstname").clear()
-        wd.find_element('name', "firstname").send_keys(firstname)
+        wd.find_element('name', "firstname").send_keys(entry.firstname)
         wd.find_element('name', "lastname").click()
         wd.find_element('name', "lastname").clear()
-        wd.find_element('name', "lastname").send_keys(lastname)
+        wd.find_element('name', "lastname").send_keys(entry.lastname)
         wd.find_element('name', "address").click()
         wd.find_element('name', "address").clear()
-        wd.find_element('name', "address").send_keys(address)
+        wd.find_element('name', "address").send_keys(entry.address)
         wd.find_element('name', "home").click()
         wd.find_element('name', "home").clear()
-        wd.find_element('name', "home").send_keys(home)
+        wd.find_element('name', "home").send_keys(entry.home)
         wd.find_element('name', "mobile").click()
         wd.find_element('name', "mobile").clear()
-        wd.find_element('name', "mobile").send_keys(mobile)
+        wd.find_element('name', "mobile").send_keys(entry.mobile)
         wd.find_element('name', "email").click()
         wd.find_element('name', "email").clear()
-        wd.find_element('name', "email").send_keys(email)
+        wd.find_element('name', "email").send_keys(entry.email)
         wd.find_element('name', "email2").click()
         wd.find_element('name', "email2").clear()
-        wd.find_element('name', "email2").send_keys(email2)
+        wd.find_element('name', "email2").send_keys(entry.email2)
         wd.find_element('name', "bday").click()
-        Select(wd.find_element('name', "bday")).select_by_visible_text(bday)
+        Select(wd.find_element('name', "bday")).select_by_visible_text(entry.bday)
         wd.find_element('xpath', "//option[@value='1']").click()
         wd.find_element('name', "bmonth").click()
         wd.find_element('xpath', "//option[@value='-']").click()
         wd.find_element('name', "bmonth").click()
-        Select(wd.find_element('name', "bmonth")).select_by_visible_text(bmonth)
+        Select(wd.find_element('name', "bmonth")).select_by_visible_text(entry.bmonth)
         wd.find_element('xpath', "//option[@value='May']").click()
         wd.find_element('name', "byear").click()
         wd.find_element('name', "byear").clear()
-        wd.find_element('name', "byear").send_keys(byear)
+        wd.find_element('name', "byear").send_keys(entry.byear)
         wd.find_element('name', "aday").click()
-        Select(wd.find_element('name', "aday")).select_by_visible_text(aday)
+        Select(wd.find_element('name', "aday")).select_by_visible_text(entry.aday)
         wd.find_element('xpath', "//div[@id='content']/form/select[3]/option[18]").click()
         wd.find_element('name', "amonth").click()
-        Select(wd.find_element('name', "amonth")).select_by_visible_text(amonth)
+        Select(wd.find_element('name', "amonth")).select_by_visible_text(entry.amonth)
         wd.find_element('xpath', "//div[@id='content']/form/select[4]/option[11]").click()
         wd.find_element('name', "ayear").click()
         wd.find_element('name', "ayear").clear()
-        wd.find_element('name', "ayear").send_keys(ayear)
+        wd.find_element('name', "ayear").send_keys(entry.ayear)
         #submit entry creation
         wd.find_element('xpath', "//input[20]").click()
 
@@ -81,9 +80,9 @@ class TestAddEntry(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, password = "secret", username = "admin")
-        self.create_entry(wd, firstname="Павел", lastname="Петровский", address="Russia, Saint-Petersburg",
+        self.create_entry(wd, Entry(firstname="Павел", lastname="Петровский", address="Russia, Saint-Petersburg",
                           home="1234578", mobile="8 921 921 92 92", email="pavel@gmail.com", email2="pavel1@gmail.com",
-                          byear="1985", ayear="1985", bmonth="May", aday="16", bday="1", amonth="October")
+                          byear="1985", ayear="1985", bmonth="May", aday="16", bday="1", amonth="October"))
         self.return_to_the_entry_list(wd)
         self.logout(wd)
 
@@ -92,9 +91,9 @@ class TestAddEntry(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, password = "secret", username = "admin")
-        self.create_entry(wd, firstname="", lastname="", address="",
+        self.create_entry(wd, Entry(firstname="", lastname="", address="",
                           home="", mobile="", email="", email2="",
-                          byear="", ayear="", bmonth="-", aday="", bday="", amonth="-")
+                          byear="", ayear="", bmonth="-", aday="", bday="", amonth="-"))
         self.return_to_the_entry_list(wd)
         self.logout(wd)
 
