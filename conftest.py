947119b7cae5ffc761a6794a77fd2dc28a8baf99
Veleria <dliavajnogo@gmail.com>
2024-01-26 12:53:00 +0300

    devide fixture into two: set and stop add check for actual fixture into conftest

--- conftest.py
+++ conftest.py
@@ -1,11 +1,26 @@
 import pytest
 from fixture.application import Application
 
+# глобальная переменная для хранения фикстуры
+fixture = None
+
 # инициализатор фикстуры
-@pytest.fixture(scope = "session")
+@pytest.fixture
 def app(request):
-    fixture =Application()
-    fixture.session.login(username="admin", password="secret")
+    global fixture
+    if fixture is None:
+        fixture =Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+# фикстура выполнится несмотря на то, что нигде не указана
+# благодаря параметру autouse
+@pytest.fixture(scope = "session", autouse = True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
