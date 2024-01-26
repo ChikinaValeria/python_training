d0a023579148ac07be9d0b4c450932fdcd4b11d2
Veleria <dliavajnogo@gmail.com>
2024-01-26 12:26:30 +0300

    add if-else construction extract fill_group_form_method extract change_field method

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,8 +1,24 @@
 from model.group import Group
 
-def test_edit_group(app):
+"""def test_edit_first_group_all_fields(app):
     app.session.login(username = "admin", password = "secret")
-    app.group.edit(Group (name ="Отредактированная группа", header ="Отредактированный заголовок", footer ="Дополненный комментарий"))
+    app.group.edit_first_group(Group (name ="Отредактированная группа", header ="Отредактированный заголовок", footer ="Дополненный комментарий"))
+    app.session.logout()"""
+
+
+def test_edit_first_group_name(app):
+    app.session.login(username = "admin", password = "secret")
+    app.group.edit_first_group(Group (name ="New name"))
     app.session.logout()
 
+def test_edit_first_group_header(app):
+    app.session.login(username = "admin", password = "secret")
+    app.group.edit_first_group(Group (header ="New header"))
+    app.session.logout()
+
+
+def test_edit_first_group_all_footer(app):
+    app.session.login(username = "admin", password = "secret")
+    app.group.edit_first_group(Group (footer ="New footer"))
+    app.session.logout()
 
