96fc7fc09d09a11609bbfc294aa3d5d3c58e7029
Veleria <dliavajnogo@gmail.com>
2024-01-26 18:01:45 +0300

    add precondition to the test_edit_entry

--- test/test_edit_entry.py
+++ test/test_edit_entry.py
@@ -1,6 +1,8 @@
 from model.entry import Entry
 
 def test_edit_first_entry_all_fields(app):
+    if app.entry.count() == 0:
+        app.entry.create(Entry(firstname='test'))
     app.entry.edit_first_entry(Entry (firstname="Иммануил", lastname="Павловский", address="Russia, Nahodka",
                                home="987654321", mobile="8 921 921 92 92", email="Ppavel@gmail.com",
                                email2="Lpavel1@gmail.com", byear="1985", ayear="1985", bmonth="May",
@@ -8,12 +10,18 @@ def test_edit_first_entry_all_fields(app):
 
 
 def test_edit_first_entry_firstname(app):
+    if app.entry.count() == 0:
+        app.entry.create(Entry(firstname='test'))
     app.entry.edit_first_entry(Entry (firstname="Афанасий"))
 
 def test_edit_first_entry_home(app):
+    if app.entry.count() == 0:
+        app.entry.create(Entry(firstname='test'))
     app.entry.edit_first_entry(Entry (home="1111111"))
 
 def test_edit_first_entry_bmonth(app):
+    if app.entry.count() == 0:
+        app.entry.create(Entry(firstname='test'))
     app.entry.edit_first_entry(Entry (bmonth="April"))
 
 
