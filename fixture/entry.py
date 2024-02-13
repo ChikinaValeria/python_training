6f64a1e7f6bf503cf3a5228d7b2e2036696723b6
Valeria Chikina <dliavajnogo@gmail.com>
2024-02-13 12:27:23 +0300

    cut spaces from emails, leave other symbols

--- fixture/entry.py
+++ fixture/entry.py
@@ -142,7 +142,7 @@ class Entry_helper:
                 #lastname = element.find_element(By.CSS_SELECTOR, "td:nth-child(2)").text
                 #id = element.find_element('name', "selected[]").get_attribute("value")
                 all_phones = cells[5].text
-                all_emails = cells[4].text
+                all_emails = cells[4].text.replace(' ', '')
                 self.entry_cache.append(Entry(firstname = firstname, lastname=lastname, id=id, address = address,
                                               all_phones_from_home_page = all_phones, all_emails_from_home_page = all_emails))
 
--- test/test_phones.py
+++ test/test_phones.py
@@ -28,13 +28,17 @@ def test_random_entry_on_home_page(app):
     random_entry = random.randrange(0, len(app.entry.get_entry_list()))
     entry_from_home_page = app.entry.get_entry_list()[random_entry]
     entry_from_edit_page = app.entry.get_entry_info_from_edit_page(random_entry)
-    print("entry_from_edit_page", entry_from_edit_page)
+    print("entry_home", entry_from_home_page)
+    print("entry_from_home_page", entry_from_home_page.all_emails_from_home_page)
+    print("entry_edit", entry_from_edit_page)
+    print("entry_from_edit_page", merge_emails_like_on_home_page(entry_from_edit_page))
 
     assert entry_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(entry_from_edit_page)
     assert entry_from_home_page.firstname == entry_from_edit_page.firstname
     assert entry_from_home_page.lastname == entry_from_edit_page.lastname
     assert entry_from_home_page.address == entry_from_edit_page.address
     assert entry_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(entry_from_edit_page)
+
 
 def merge_phones_like_on_home_page(entry):
     # удаляем из списка все None
@@ -49,8 +53,10 @@ def merge_emails_like_on_home_page(entry):
     # удаляем из списка все None
     # используем лямбда-выражение, чтобы не применять clear ко всем элементам списка поотдельности
     # избавляемся от пустых строк с помощью filter
-    return "\n".join(filter (lambda x : x != "", (map (lambda x: clear(x),filter (lambda x: x is not None,
-                                                                                  [entry.email, entry.email2])))))
+    return "\n".join(filter (lambda x : x != "", (map (lambda x: clear_emails(x),filter (lambda x: x is not None,
+                                                                                  [entry.email, entry.email2, entry.email3])))))
+def clear_emails(s):
+    return re.sub("[ ]", "", s)
 def clear(s):
     return re.sub("[() -]", "", s)
 
