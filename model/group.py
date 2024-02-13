7da6ea7c6c3c4611b346376508ba88b41874b404
Valeria Chikina <dliavajnogo@gmail.com>
2024-02-13 13:09:40 +0300

    add parameters to group creation test

--- model/group.py
+++ model/group.py
@@ -8,7 +8,7 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return "Group(%s, %s)" % (self.name, self.id)
+        return "Group(%s, %s, %s, %s)" % (self.name, self.id, self.header, self.footer)
 
     def __eq__(self, other):
         return self.name == other.name and (self.id is None or other.id is None or self.id == other.id)
